7f7412d5a63c7110b0381d6e38d934653aa903fd
Raphael <rapahel.yohannes@gmail.com>
2022-08-19 16:02:10 -0500

    complete bonus phase

---
+++ frontend/src/components/LoginFormModal/index.js
@@ -0,0 +1,22 @@
+// frontend/src/components/LoginFormModal/index.js
+//Updates from Bonus
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import LoginForm from './LoginForm';
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Log In</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
