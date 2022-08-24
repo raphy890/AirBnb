3b4c2c401c2089bd08c3d23fbc2bf770fb1c85d3
Raphael <rapahel.yohannes@gmail.com>
2022-08-24 11:59:16 -0500

    updated imports for Redirect

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,7 +1,7 @@
 // frontend/src/components/SignupFormPage/index.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {Redirect} "react-router-dom"
+import {Redirect} from "react-router-dom"
 import * as sessionActions from "../../store/session";
 import './SignupForm.css';
 
