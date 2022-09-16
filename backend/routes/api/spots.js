4e996b2f3139d82ff15759c640cf42035143e5f7
Raphael <rapahel.yohannes@gmail.com>
2022-09-16 18:05:12 -0500

    again

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -141,8 +141,8 @@ router.get('/:spotId', async (req, res) => {
 
 //POST/CREATE a New Spot - COMPLETE
 router.post('/', async (req, res) => {
-  // const { address, city, state, country, lat, lng, name, description, price } = req.body //deconstruct attributes from the body
-  const { address, city, state, country, name, description, price } = req.body //deconstruct attributes from the body
+  const { address, city, state, country, lat, lng, name, description, price } = req.body //deconstruct attributes from the body
+  // const { address, city, state, country, name, description, price } = req.body //deconstruct attributes from the body
 
   // console.log(req.body);
 
